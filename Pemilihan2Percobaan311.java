89a5aa9dde35efe69ec5d0982b6e583d5316751f
Ghazwan Ababil <ababilghazwan@gmail.com>
2024-10-04 06:49:02 +0700

    Modifikasi percobaan 3

--- Pemilihan2Percobaan311.java
+++ Pemilihan2Percobaan311.java
@@ -12,7 +12,7 @@ public class Pemilihan2Percobaan311 {
         System.out.print("Masukkan Besarnya Penghasilan : ");
         penghasilan = input11.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000)
                 pajak = 0.1;
             else if (penghasilan <= 3000000)
@@ -21,7 +21,7 @@ public class Pemilihan2Percobaan311 {
                 pajak = 0.2;
             gajiBersih = (int) (penghasilan - (penghasilan * pajak));
             System.out.println("Penghasilan Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000)
                 pajak = 0.15;
             else if (penghasilan <= 3500000)
